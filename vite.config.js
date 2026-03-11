e00835dfa06208dfef802842d56290fb69b958a4
YuSeong19 <yuseong1994@hotmail.com>
2026-03-11 19:44:19 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,26 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      strategies: 'injectManifest',
-      srcDir: 'public',
-      filename: 'sw.js',
-      injectManifest: {
+      strategies: 'generateSW',
+      workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/.*\.firebaseio\.com\/.*/i,
+            handler: 'NetworkFirst',
+            options: { cacheName: 'firebase-realtime', networkTimeoutSeconds: 10 },
+          },
+          {
+            urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts',
+              expiration: { maxEntries: 20, maxAgeSeconds: 60 * 60 * 24 * 365 },
+            },
+          },
+        ],
       },
+      includeAssets: ['icon-*.png', 'apple-touch-icon.png'],
       manifest: {
         name: 'YuGrill Stock',
         short_name: 'YuGrill',
@@ -24,14 +38,18 @@ export default defineConfig({
         start_url: '/',
         lang: 'th',
         icons: [
-          { src: '/icon-72x72.png',   sizes: '72x72',   type: 'image/png', purpose: 'any maskable' },
-          { src: '/icon-96x96.png',   sizes: '96x96',   type: 'image/png', purpose: 'any maskable' },
-          { src: '/icon-128x128.png', sizes: '128x128', type: 'image/png', purpose: 'any maskable' },
-          { src: '/icon-144x144.png', sizes: '144x144', type: 'image/png', purpose: 'any maskable' },
-          { src: '/icon-152x152.png', sizes: '152x152', type: 'image/png', purpose: 'any maskable' },
-          { src: '/icon-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
-          { src: '/icon-384x384.png', sizes: '384x384', type: 'image/png', purpose: 'any maskable' },
-          { src: '/icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+          { src: 'icon-72x72.png',   sizes: '72x72',   type: 'image/png', purpose: 'any maskable' },
+          { src: 'icon-96x96.png',   sizes: '96x96',   type: 'image/png', purpose: 'any maskable' },
+          { src: 'icon-128x128.png', sizes: '128x128', type: 'image/png', purpose: 'any maskable' },
+          { src: 'icon-144x144.png', sizes: '144x144', type: 'image/png', purpose: 'any maskable' },
+          { src: 'icon-152x152.png', sizes: '152x152', type: 'image/png', purpose: 'any maskable' },
+          { src: 'icon-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'any maskable' },
+          { src: 'icon-384x384.png', sizes: '384x384', type: 'image/png', purpose: 'any maskable' },
+          { src: 'icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+        ],
+        shortcuts: [
+          { name: 'สต๊อค', short_name: 'สต๊อค', url: '/?page=stock', icons: [{ src: 'icon-96x96.png', sizes: '96x96' }] },
+          { name: 'ประวัติ', short_name: 'ประวัติ', url: '/?page=log', icons: [{ src: 'icon-96x96.png', sizes: '96x96' }] },
         ],
       },
     }),
